e17515376b390493e2610b1720b370c4fe754e53
taeseung-yoo <remicon99@gmail.com>
2024-05-17 18:08:02 +0900

    버튼 컴포넌트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { Global, ThemeProvider } from '@emotion/react';
-import Main from '@pages/main';
 import GlobalStyle from '@styles/global';
 import theme from '@styles/theme';
+import { Button } from '@components';
+import styled from '@emotion/styled';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyle} />
-      <Main />
+      <MainContainer>
+        <Button label="primary" variant={'primary'} />
+        <Button label="outline" variant={'outline'} />
+        <Button label="disable" variant={'disable'} />
+        <Button label="small" size={'small'} />
+        <Button label="medium" size={'medium'} />
+        <Button label="large" size={'large'} />
+      </MainContainer>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+const MainContainer = styled.div`
+  display: flex;
+  width: 15rem;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 2rem;
+`;
--- src/components/inputForm/styles.ts
+++
@@ -1,3 +0,0 @@
-import styled from '@emotion/styled';
-
-export const InputFormContainer = styled.div``;
