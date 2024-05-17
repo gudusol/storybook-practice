cf32da6b36a4d30ab6ede80710176f97de87f3cb
taeseung-yoo <remicon99@gmail.com>
2024-05-17 21:50:58 +0900

    form 추가

---
+++ src/components/Form/styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const FormContainer = styled.div`
+  width: 30rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding: 2rem;
+`;
